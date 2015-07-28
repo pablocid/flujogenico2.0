60a0ceadc0513ba4d5e7d090cc903d1246a4fd31
pablocid <pabloagronomo@gmailcom>
2015-07-28 16:03:39 -0300

    init state

---
+++ server/config/environment/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Test specific configuration
+// ===========================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/flujogenico20-test'
+  }
+};
