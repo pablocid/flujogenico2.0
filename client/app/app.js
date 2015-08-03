e626f0772bcdc0bff111636021512a1737d8e243
Pablo Cid <pabloagronomo@gmail.com>
2015-08-03 06:12:42 -0300

    Diseño grafico aceptado

--- client/app/app.js
+++ client/app/app.js
@@ -15,7 +15,59 @@ angular.module('flujogenico20App', [
     $locationProvider.html5Mode(true);
     $httpProvider.interceptors.push('authInterceptor');
   })
-
+  .config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('flora')
+      .primaryPalette('light-green', {
+        'default': '800', // by default use shade 400 from the pink palette for primary intentions
+        'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+        'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+        'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+      })
+      // If you specify less than all of the keys, it will inherit from the
+      // default shades
+      .accentPalette('purple', {
+        'default': '200' // use shade 200 for default, and keep all other shades the same
+      });
+    $mdThemingProvider.theme('fauna')
+      .primaryPalette('orange', {
+        'default': '800', // by default use shade 400 from the pink palette for primary intentions
+        'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+        'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+        'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+      })
+      // If you specify less than all of the keys, it will inherit from the
+      // default shades
+      .accentPalette('purple', {
+        'default': '200' // use shade 200 for default, and keep all other shades the same
+      });
+    $mdThemingProvider.theme('cira')
+      .primaryPalette('red', {
+        'default': '500', // by default use shade 400 from the pink palette for primary intentions
+        'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+        'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+        'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+      })
+      // If you specify less than all of the keys, it will inherit from the
+      // default shades
+      .accentPalette('purple', {
+        'default': '200' // use shade 200 for default, and keep all other shades the same
+      });
+    $mdThemingProvider.theme('escala')
+      .primaryPalette('blue', {
+        'default': '500', // by default use shade 400 from the pink palette for primary intentions
+        'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+        'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+        'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+      })
+      // If you specify less than all of the keys, it will inherit from the
+      // default shades
+      .accentPalette('purple', {
+        'default': '500' // use shade 200 for default, and keep all other shades the same
+      })
+      .backgroundPalette('blue',{
+        'default':'500'
+      });
+  })
   .factory('authInterceptor', function ($rootScope, $q, $cookieStore, $location) {
     return {
       // Add authorization token to headers
