e81bd1c2810222588cc6f0a2781d9249780e1fbd
pablocid <pabloagronomo@gmailcom>
2015-09-08 16:13:47 -0300

    Arreglo de Assets con js

---
+++ client/app/application/results/results.local.controller.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('flujogenico20App')
+    .controller('resultsLocalController', function () {
+    var self = this;
+    self.species = ['agrostis_capillaris', 'agrostis_leptotricha', 'agrostis_magellanica','brassica_napus'];
+    /*
+     self.selectionInfo = function(a){
+     self.showSelection = a;
+     };
+     $interval( function(){
+     console.log(self.showSelection);
+     }, 500);
+     */
+
+  });
