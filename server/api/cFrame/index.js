a3ef86fb2059502e65d20c97e696fca59ba1e200
pablocid <pabloagronomo@gmailcom>
2015-10-25 17:48:23 -0300

    cframe backend gdrive - ajustes en directiva : progres linear bar

--- server/api/cFrame/index.js
+++ server/api/cFrame/index.js
@@ -6,5 +6,5 @@ var controller = require('./cFrame.controller.js');
 var router = express.Router();
 
 router.get('/', controller.index);
-
+router.get('/:url', controller.getIframe);
 module.exports = router;
